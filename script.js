ff79dfe8b815e68e6dfdee65ca533dd01ee26c3d
filippo <riccardo.gabba02@gmail.com>
2023-10-12 15:18:58 +0200

    added email box, button and input text

---
+++ script.js
@@ -0,0 +1,16 @@
+
+button.addEventListener('click', function () {
+
+    const emailCheck = document.getElementById('email').value;
+    const emailList = [
+        "riccardo.gabba02@gmail.com"
+    ]
+
+
+
+    if(validEmail.includes(emailCheck)){
+        
+    }
+
+
+})
